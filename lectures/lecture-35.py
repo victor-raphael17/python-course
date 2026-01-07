684d192e44ff66e3bac2a74c8733d633f38fecb5
victor-raphael17 <victorraphaellima2005@gmail.com>
2026-01-06 22:57:26 -0300

    /lectures-py-file to / lectures

---
+++ lectures/lecture-35.py
@@ -0,0 +1,12 @@
+# Tuples
+
+# tuple is immutable
+# allow duplicates
+# tuple is indexed
+
+#        Ordered | Duplicates | Indexed | Mutable
+#  list     V           V          V         V
+#  tuple    V           V          V         X
+#
+
+my_tuple = (10, 30, 20)
---
+++ lectures/lecture-36.py
@@ -0,0 +1,11 @@
+# Unordered collection of unique values
+
+# set is unique (no duplicates)
+
+# set is faster than list because the memory allocation is more efficent
+
+my_set = {10, 30, 20}
+
+print(my_set)
+
+
